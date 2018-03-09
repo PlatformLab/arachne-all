9b5c3cfdd68ac8710a470f13d7e90cbba6ab101c
Henry Qin <hq6@cs.stanford.edu>
2018-03-08 17:25:26 -0700

    Add an initial attempt to port to FreeBSD

--- buildAll.sh
+++ buildAll.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 targets='PerfUtils CoreArbiter Arachne ArachnePerfTests arachne-sosp2017-benchmarks'
 for dir in $targets; do
     pushd $dir > /dev/null
-    make
+    gmake
     popd > /dev/null
 done
